24fa89697d56cb5c1866c77beead9bc01eba1d2b
11292-stella <stella.marucelli@gmail.com>
2025-06-05 16:40:57 +0200

    altro

--- src/main/java/esercizio/pizza_jpa/Runner.java
+++ src/main/java/esercizio/pizza_jpa/Runner.java
@@ -1,10 +1,17 @@
 package esercizio.pizza_jpa;
 
+import esercizio.pizza_jpa.Entities.Drink;
+import esercizio.pizza_jpa.Entities.Pizza;
+import esercizio.pizza_jpa.Entities.Tavolo;
+import esercizio.pizza_jpa.Entities.Topping;
 import esercizio.pizza_jpa.repository.*;
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 public class Runner implements CommandLineRunner {
 
@@ -30,6 +37,29 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Topping tomato = ctx.getBean("tomato", Topping.class);
+        Topping mozzarella = ctx.getBean("Mozzarella", Topping.class);
+        Topping prosciuttoCotto = ctx.getBean("prosciuttoCotto", Topping.class);
+
+        toppingRepository.saveAll(Arrays.asList(tomato,mozzarella,prosciuttoCotto));
+
+        Pizza margherita = ctx.getBean("margherita", Pizza.class);
+        Pizza primavera = ctx.getBean("primavera", Pizza.class);
+
+        pizzaRepository.saveAll(Arrays.asList(margherita,primavera));
+
+        Drink water = ctx.getBean("water", Drink.class);
+        Drink cocaCola = ctx.getBean("cocaCola", Drink.class);
+
+        drinkRepository.saveAll(Arrays.asList(water,cocaCola));
+
+        Tavolo t1 = ctx.getBean("t1", Tavolo.class);
+        Tavolo t2 = ctx.getBean("t2", Tavolo.class);
+
+        tavoliRepository.saveAll(Arrays.asList(t1,t2));
+
+
+        
 
 
 
